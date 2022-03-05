3899d79a2c2d5c16520c2d2bbc5d32c1992964f2
Tiffany Asbell <tasbell26@gmail.com>
2022-03-04 18:45:46 -0600

    changes2.0

--- HiddenOddities1/index.js
+++
@@ -1,3 +0,0 @@
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("navBar").classList.toggle("hidden--mobile nav-links");
-});
---
+++ index.js
@@ -0,0 +1,3 @@
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("navBar").classList.toggle("hidden--mobile nav-links");
+});
