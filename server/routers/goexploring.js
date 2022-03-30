4d2efd5d5f888b9b30f76a13b6e2ca90e8616def
Tiffany Asbell <tasbell26@gmail.com>
2022-03-30 17:23:18 -0500

    maybe

--- server/routers/goexploring.js
+++
@@ -1,7 +0,0 @@
-// routers are always plural and representing the route your using
-const { Router } = require("express");
-const Pizza = require("../models/Goexplorings");
-
-const router = Router();
-
-module.exports = router;
